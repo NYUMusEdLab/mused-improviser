96d51b2a3aa86aa4d247eca9a1d9b8e63d3dba6b
algomusic <algorithmicmusic@gmail.com>
2016-11-19 00:18:14 +1000

    Updated to use new music data structures

--- tone-array-player.js
+++ tone-array-player.js
@@ -3,30 +3,51 @@ var synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
 var part = new Tone.Part(function (time, noteData){
 	synth.triggerAttackRelease(noteData[0], noteData[1], time, noteData[2]);
 }, []);
+ // onset and durations in 8th notes
+var headData = [
+	{onset: 0, pitch: 72, dur: 3},
+	{onset: 3, pitch: 71, dur: 1},
+	{onset: 4, pitch: 69, dur: 2},
+	{onset: 6, pitch: 67, dur: 1},
+	{onset: 7, pitch: 65, dur: 2},
+	{onset: 9, pitch: 67, dur: 3},
+	{onset: 12, pitch: 69, dur: 2},
+	{onset: 14, pitch: 72, dur: 2},
+	{onset: 16, pitch: 71, dur: 3},
+	{onset: 19, pitch: 69, dur: 1},
+	{onset: 20, pitch: 67, dur: 2},
+	{onset: 22, pitch: 65, dur: 1},
+	{onset: 23, pitch: 64, dur: 9}
+];
 
-var head = [
+var improvData = [
+	{onset: 0, pitch: 72, dur: 3},
+	{onset: 3, pitch: 71, dur: 1},
+	{onset: 4, pitch: 69, dur: 2},
+	{onset: 6, pitch: 67, dur: 1},
+	{onset: 7, pitch: 65, dur: 2},
+	{onset: 9, pitch: 67, dur: 3},
+	{onset: 12, pitch: 69, dur: 2},
+	{onset: 14, pitch: 72, dur: 2},
+	{onset: 16, pitch: 71, dur: 3},
+	{onset: 19, pitch: 69, dur: 1},
+	{onset: 20, pitch: 67, dur: 2},
+	{onset: 22, pitch: 65, dur: 1},
+	{onset: 23, pitch: 64, dur: 9}
+];
+
+//var headPitches = [72, 0, 0, 71, 69, 0, 67, 65, 0, 67, 0, 0, 69, 0, 72, 0, 71, 0, 0, 69, 67, 0, 65, 64, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+var perfData = [
 	{onset: 0, pitch: 72, dur: 3},
 	{onset: 4, pitch: 71, dur: 3},
 // etc.
 ];
 
-var improv = [
-	{onset: 0, pitch: 72, dur: 3},
-	{onset: 4, pitch: 71, dur: 3},
-// etc.
-];
-
-var perf = [
-	{onset: 0, pitch: 72, dur: 3},
-	{onset: 4, pitch: 71, dur: 3},
-// etc.
-];
-
-head[0].pitch;
-
-var headPitches = [72, 0, 0, 71, 69, 0, 67, 65, 0, 67, 0, 0, 69, 0, 72, 0, 71, 0, 0, 69, 67, 0, 65, 64, 0, 0, 0, 0, 0, 0, 0, 0];
-var headDurations = [];
-var improvPitches = [72, 0, 0, 71, 69, 0, 67, 65, 0, 67, 0, 0, 69, 0, 72, 0, 71, 0, 0, 69, 67, 0, 65, 64, 0, 0, 0, 0, 0, 0, 0, 0];
+//var headPitches = [72, 0, 0, 71, 69, 0, 67, 65, 0, 67, 0, 0, 69, 0, 72, 0, 71, 0, 0, 69, 67, 0, 65, 64, 0, 0, 0, 0, 0, 0, 0, 0];
+//var headDurations = [];
+//var improvPitches = [72, 0, 0, 71, 69, 0, 67, 65, 0, 67, 0, 0, 69, 0, 72, 0, 71, 0, 0, 69, 67, 0, 65, 64, 0, 0, 0, 0, 0, 0, 0, 0];
 
 var chords = [[9, 0, 4, 7], [9, 0, 4, 7], [9, 0, 4, 7], [9, 0, 4, 7],
 		[2, 5, 9, 0], [2, 5, 9, 0], [2, 5, 9, 0], [2, 5, 9, 0],
@@ -34,52 +55,55 @@ var chords = [[9, 0, 4, 7], [9, 0, 4, 7], [9, 0, 4, 7], [9, 0, 4, 7],
 		[0, 4, 7, 10], [0, 4, 7, 10], [0, 4, 7, 11], [0, 4, 7, 11]]; // per beat
 
 function fillPart(data) {
-	var pos = 0;
-	var posB = 0;
-	var posQ = 0;
-	var posS = 0;
-
 	part.removeAll();
-
 	for (var i=0; i<data.length; i++) {
-		var time = posB + ":" + posQ + ":" + posS;
-		var pitch = Tone.Frequency(data[i], "midi").eval();
+		var pos = data[i].onset * 2;
+		var time = Math.floor(pos/16) + ":" + Math.floor(pos/4)%4 + ":" + pos%4;
+		var pitch = Tone.Frequency(data[i].pitch, "midi").eval();
 		var vel = Math.random() / 3 * 2 + 0.3;
-		var len = 2;
-		var dur = "16n";
-		if (i+1 < data.length && data[i + 1] === 0) {
-		 	len += 2;
-		}
-		if (len === 2) dur = "16n";
-		if (len === 4) dur = "8n";
-		if (len === 6) dur = "6n";
-		if (len === 8) dur = "4n";
-		if (len === 16) dur = "2n";
-		if (len > 16) dur = "1n";
-		//console.log([time, pitch, dur, vel, pos]);
-	  if (data[i] > 0) part.add(time, [pitch, dur, vel]);
-	  pos += 2;
-	  posB = Math.floor(pos/16);
-	  posQ = Math.floor(pos/4)%4;
-	  posS = pos%4;
+		var len = data[i].dur;
+		// divide?
+		console.log(i);
+		// //console.log([time, pitch, dur, vel, pos]);
+	  part.add(time, [pitch, convertDur(len), vel]);
 	}
+}
+
+function convertDur(len) {
+	var dur = "16n";
+	if (len === 1) dur = "16n";
+	if (len === 2) dur = "8n";
+	if (len === 3) dur = "6n";
+	if (len >= 4 && len <= 7) dur = "4n";
+	if (len >= 8 ) dur = "2n";
+	return dur;
 }
 
 function genImprov() {
-	// generate an imporvisation
+	//generate an improvisation
 	console.log("composing...");
-	for (var i=0; i<headPitches.length; i++) {
-		var pitchSet = [0, 2, 4, 7, 9];
+	for (var i=0; i<headData.length; i++) {
+		if (i < (headData.length - 1) && (headData[i].onset < 32) &&
+			((headData[i + 1].onset - headData[i].onset)%2 === 0) && (Math.random() < 0.99)) {
+				console.log("half");
+				improvData.push([headData[i].onset + ((headData[i + 1].onset - headData[i].onset) / 2),
+					[headData[i].pitch, convertDur(headData[i].dur / 2, 0.5)]]);
+				//part.add(data[i].onset + ((data[i + 1].onset - data[i].onset) / 2),
+		 		//	[pitch, convertDur(len  / 2), Math.random() / 3 * 2 + 0.3]);
+		 	//len = len / 2;
+		}
+		var pitchSet = [];
 		if (i%2 === 0) {
 			pitchSet = chords[Math.floor(i / 2)];
+		} else {
+			pitchSet = pitchSet.concat([0, 2, 4, 7, 9]);
 		}
-		if (headPitches[i] > 0) {
-			improvPitches[i] = quantize(headPitches[i] + Math.round(Math.random() * 4 - 2), pitchSet);
+		if (headData[i].pitch > 0) {
+			improvData[i].pitch = quantize(headData[i].pitch + Math.round(Math.random() * 4 - 2), pitchSet);
 		}
 	}
 }
-// use JS timer to replay the part after a specified interval (may not be an immediate loop!)
-
+// adjust pitch to match one of the supplied pitch classes
 function quantize (val, scale) {
 	while (scale.indexOf(val%12) === -1) {
 		val -= 1;
@@ -87,12 +111,9 @@ function quantize (val, scale) {
 	return val;
 }
 
-Tone.Transport.bpm.value = 120;
+Tone.Transport.bpm.value = 124;
 Tone.Transport.timeSignature = [4, 4];
-Tone.Transport.swing = 0.5;
-// Tone.Transport.loop = true;
-// Tone.Transport.loopStart = 0;
-// Tone.Transport.loopEnd = "4:0:0";
+Tone.Transport.swing = 0.0;
 Tone.Transport.start();
 part.loop = true;
 part.loopStart = 0;
@@ -103,21 +124,16 @@ var isImprov = true;
 
 Tone.Transport.scheduleRepeat(function () {
 	if (isImprov) { // alternate between original and improvised versions
-		fillPart(headPitches);
+		fillPart(headData);
 		isImprov = false;
 	} else {
 		genImprov();
-		fillPart(improvPitches);
+		fillPart(improvData);
 		isImprov = true;
 	}
-	console.log(Tone.Transport.position);
+	//console.log(Tone.Transport.position);
 	if (firstTime) {
 		setTimeout(function(){part.start()}, 1);
 		firstTime = false;
 	}
 }, "4m");
-
-
-//setTimeout(function(){part.start()}, 100);
-
-//looper(0);
